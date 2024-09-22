49f0cd5ea24a9d5ed653c2d6b90a26415ec3b3bd
SrDiegoH <diegohenrique4222@gmail.com>
2024-09-22 03:43:38 -0300

    Merge sources data

--- index.py
+++ index.py
@@ -18,17 +18,17 @@ CACHE_EXPIRY = timedelta(days=1)
 
 FUNDSEXPLORER_SOURCE = 'fundsexplorer'
 FIIS_SOURCE = 'fiis'
+FUNDAMENTUS_SOURCE = 'fundamentus'
 
 def request_fii_by(ticker, source):
     if source == FUNDSEXPLORER_SOURCE:
-        print("----> [request_fii_by] Calling fundsexplorer")
         return get_data_from_fundsexplorer_by(ticker)
     elif source == FIIS_SOURCE:
-        print("----> [request_fii_by] Calling fiis")
         return get_data_from_fiis_by(ticker)
+    elif source == FUNDAMENTUS_SOURCE:
+        return get_data_from_fundamentus_by(ticker)
 
-    print("----> [request_fii_by] Calling fundamentus")
-    return get_data_from_fundamentus_by(ticker)
+    return get_data_from_all_by(ticker)
 
 def get_data_from_fundamentus_by(ticker):
     url = f'https://fundamentus.com.br/detalhes.php?papel={ticker}'
@@ -46,34 +46,44 @@ def get_data_from_fundamentus_by(ticker):
     return convert_fundamentus_data(response)
 
 def convert_fundamentus_data(data):
+    def generate_link(cnpj):
+        return f'https://fnet.bmfbovespa.com.br/fnet/publico/abrirGerenciadorDocumentosCVM?cnpjFundo={cnpj}#'
+
+    def get_dividends(distributed_dividends, total_quotas):
+        return distributed_dividends / total_quotas / 12
+
+    distributed_dividends = float(get_substring(data, 'Rend. Distribuído</span>', '</span>'))
+    total_quotas = float(get_substring(data, 'Nro. Cotas</span>', '</span>'))
+
     return {
-        "fundname": get_substring(data, 'Nome</span>', '</span>'),
-        "setor_atuacao": None,
-        "segmento_ambima":get_substring(data, 'Mandato</span>', '</span>'),
-        "valor_caixa":  get_substring(get_substring(data, 'Caixa\',', '}', False), '[', ']', False),
-        "valor": get_substring(data, 'Cotação</span>', '</span>'),
-        "liquidezmediadiaria": get_substring(data, 'Vol $ méd (2m)</span>', '</span>'),
+        "nome": get_substring(data, 'Nome</span>', '</span>'),
+        "gestao": get_substring(data, 'Gestão</span>', '</span>'),
+        "tipo": None,
+        "segmento": get_substring(data, 'Mandato</span>', '</span>'),
+        "atuacao": None,
+        "valor_caixa": get_substring(get_substring(data, 'Caixa\',', '}', False), '[', ']', False),
+        "valor_ativos": get_substring(data, '>Ativos</span>', '</span>'),
+        "valor_mercado": get_substring(data, 'Valor de mercado</span>', '</span>'),
+        "valor_patrimonio_liquido": get_substring(data, 'Patrim Líquido</span>', '</span>'),
+        "valor_cotacao": get_substring(data, 'Cotação</span>', '</span>'),
+        "liquidez": get_substring(data, 'Vol $ méd (2m)</span>', '</span>'),
         "pvp": get_substring(data, 'P/VP</span>', '</span>'),
+        "ffoy": get_substring(data, 'FFO Yield</span>', '</span>'),
         "dy": get_substring(data, 'Div. Yield</span>', '</span>'),
-        "dividendos_12_meses": get_substring(data, 'Rend. Distribuído</span>', '</span>'),
-        "lastdividend": get_substring(data, 'Dividendo/cota</span>', '</span>'),
-        "patrimonio": get_substring(data, 'Patrim Líquido</span>', '</span>'),
+        "dividendos_12_meses": get_dividends(distributed_dividends, total_quotas),
+        "ultimo_dividendo": get_substring(data, 'Dividendo/cota</span>', '</span>'),
         "valorizacao_12_meses": get_substring(data, '12 meses</span>', '</span>'),
-        "valorizacao_mes": get_substring(data, 'Mês</span>', '</span>'),
+        "valorizacao_ultimo_mes": get_substring(data, 'Mês</span>', '</span>'),
         "min_52_semanas": get_substring(data, 'Min 52 sem</span>', '</span>'),
         "max_52_semanas": get_substring(data, 'Max 52 sem</span>', '</span>'),
-        "assets_number": get_substring(data, 'Qtd imóveis</span>', '</span>'),
+        "qnt_imoveis": get_substring(data, 'Qtd imóveis</span>', '</span>'),
         "taxas": None,
         "vacancia": get_substring(data, 'Vacância Média</span>', '</span>'),
-        "firstdate": None,
-        "numero_cotas": get_substring(data, 'Nro. Cotas</span>', '</span>'),
-        "valormercado": get_substring(data, 'Valor de mercado</span>', '</span>'),
-        "publicoalvo": None,
-        "prazoduracao": None,
-        "gestao": get_substring(data, 'Gestão</span>', '</span>'),
-        "ffoy": get_substring(data, 'FFO Yield</span>', '</span>'),
-        "link": get_substring(data, 'abrirGerenciadorDocumentosCVM?cnpjFundo=', '">Pesquisar Documentos', False),
-        "ativos": get_substring(data, '>Ativos</span>', '</span>')
+        "total_cotas_emitidas": total_quotas,
+        "data_inicio": None,
+        "publico_alvo": None,
+        "prazo": None,
+        "link": generate_link(get_substring(data, 'abrirGerenciadorDocumentosCVM?cnpjFundo=', '">Pesquisar Documentos', False))
     }
 
 def get_data_from_fundsexplorer_by(ticker):
@@ -96,53 +106,49 @@ def get_data_from_fundsexplorer_by(ticker):
 
 def convert_fundsexplorer_data(data):
     return {
-        "fundname": data["name"],
-        "setor_atuacao": data["setor_atuacao"],
-        "segmento_ambima": data["segmento_ambima"],
+        "nome": data["name"],
+        "gestao": data["gestao"],
+        "tipo": data["setor_atuacao"],
+        "segmento": data["segmento_ambima"],
+        "atuacao": data["segmento_atuacao"],
         "valor_caixa": data["valor_caixa"],
-        "valor": data["valor"],
-        "liquidezmediadiaria": data["liquidezmediadiaria"],
+        "valor_mercado": data["valormercado"],
+        "valor_patrimonio_liquido": data["patrimonio"],
+        "valor_cotacao": data["valor"],
+        "liquidez": data["liquidezmediadiaria"],
         "pvp": data["pvp"],
+        "ffoy": None,
         "dy": data["dy"],
         "dividendos_12_meses": data["dividendos_12_meses"],
-        "lastdividend": data["lastdividend"],
-        "patrimonio": data["patrimonio"],
+        "ultimo_dividendo": data["lastdividend"],
         "valorizacao_12_meses": data["valorizacao_12_meses"],
-        "valorizacao_mes": data["valorizacao_12_meses"],
+        "valorizacao_ultimo_mes": data["valorizacao_mes"],
         "min_52_semanas": data["min_52_semanas"],
         "max_52_semanas": data["max_52_semanas"],
-        "assets_number": data["assets_number"],
+        "qnt_imoveis": data["assets_number"],
         "taxas": data["taxas"],
         "vacancia": data["vacancia"],
-        "firstdate": data["firstdate"],
-        "numero_cotas": data["numero_cotas"],
-        "valormercado": data["valormercado"],
-        "publicoalvo": data["publicoalvo"],
-        "prazoduracao": data["prazoduracao"],
-        "gestao": data["gestao"]
+        "total_cotas_emitidas": data["numero_cotas"],
+        "data_inicio": data["firstdate"],
+        "publico_alvo": data["publicoalvo"],
+        "prazo": data["prazoduracao"],
+        "link": None
     }
 
-def get_data_from_fiis_by(ticker):
-    base_url = "www.fiis.com.br"
-    url_path = f"/{ticker}"
+def get_data_from_all_by(ticker):
+    data_fundamentus = get_data_from_fundamentus_by(ticker)
+    data_fundsexplorer = get_data_from_fundsexplorer_by(ticker)
 
-    headers = {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7"
-    }
+    data_merge = {}
 
-    connection = httplib.HTTPSConnection(base_url)
-    connection.request(method='POST', url=url_path, body=None, headers=headers)
+    for key, value in data_fundamentus.items():
+        if key in data_fundsexplorer and not value:
+            data_merge[key] = data_fundsexplorer[key]
+            continue
 
-    response = connection.getresponse().read()
+        data_merge[key] = value
 
-    connection.close()
-
-    web_page = BeautifulSoup(response)
-
-    segment = web_page.select_one("#carbon_fields_fiis_informations-2 > div.moreInfo.wrapper > p:nth-child(7) > b").text.strip()
-
-    return segment
+    return data_merge
 
 def request_get(url, headers=None):
     response = requests.get(url, headers=headers)
@@ -159,16 +165,12 @@ def get_substring(text, start_text, end_text, should_remove_tags=True):
     return re.sub(r'<[^>]*>', '', cutted_text) if should_remove_tags else cutted_text
 
 def read_cache(ticker, should_clear_cache):
-    print(f'-----> [read_cache] Reading: {ticker}, Clear cache: {should_clear_cache} - Exists file: {os.path.exists(CACHE_FILE)}')
     if not os.path.exists(CACHE_FILE):
-      print(f'-----> [read_cache] File not found')
-      return None
+        return None
 
     if should_clear_cache:
-      print(f'-----> [read_cache] Cleaning cache')
-      clear_cache(ticker)
-      print(f'-----> [read_cache] Cache cleaned')
-      return None
+        clear_cache(ticker)
+        return None
 
     control_clean_cache = False
     with open(CACHE_FILE, 'r') as cache_file:
@@ -176,49 +178,37 @@ def read_cache(ticker, should_clear_cache):
             if not line.startswith(ticker):
                 continue
 
-            print(f'------> [read_cache] Ticker found: {line}')
             cached_ticker, cached_datetime, data = line.strip().split('#@#')
 
             cached_date = datetime.strptime(cached_datetime, '%Y-%m-%d %H:%M:%S')
 
-            print(f'------> [read_cache] Expiration =>  Now: {datetime.now()}, Cached: {cached_date}, exp: {CACHE_EXPIRY}, Minor: {datetime.now() - cached_date} = {datetime.now() - cached_date <= CACHE_EXPIRY}')
             if datetime.now() - cached_date <= CACHE_EXPIRY:
-                print(f'------> [read_cache] Returning data: {cached_ticker}, {cached_datetime}, {data}')
                 return data
 
             control_clean_cache = True
             break
 
     if control_clean_cache:
-      print(f'-----> [read_cache] Cleaning cache')
-      clear_cache(ticker)
-      print(f'-----> [read_cache] Cache cleaned')
+        clear_cache(ticker)
 
     return None
 
 def clear_cache(ticker):
-    print(f'-----> [clear_cache] Clearing: {ticker}')
     with open(CACHE_FILE, 'r') as cache_file:
         lines = cache_file.readlines()
-    print(f'-----> [clear_cache] Lines: {lines}')
+
     with open(CACHE_FILE, 'w') as cache_file:
         for line in lines:
             if not line.startswith(ticker):
-              cache_file.write(line)
-    print(f'-----> [clear_cache] Cleared')
+                cache_file.write(line)
 
 def write_to_cache(ticker, data):
-    print(f'-----> [write_to_cache] Writing: {ticker}, {data}')
     with open(CACHE_FILE, 'a') as cache_file:
         cache_file.write(f"{ticker}#@#{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}#@#{data}\n")
-    print(f'-----> [write_to_cache] Writed')
 
 def delete_cache():
-    print(f'-----> [delete_cache] Deleting: {CACHE_FILE}, {os.path.exists(CACHE_FILE)}')
     if os.path.exists(CACHE_FILE):
-        print(f'------> [delete_cache] File found, deleting...')
         os.remove(CACHE_FILE)
-        print(f'------> [delete_cache] Deleted')
 
 @app.route('/fii/<ticker>', methods=['GET'])
 def get_fii_data_by(ticker):
@@ -226,33 +216,21 @@ def get_fii_data_by(ticker):
     should_clear_cache = request.args.get('should_clear_cache', '0').lower() in VALID_BOOL_VALUES
     should_use_cache = request.args.get('should_use_cache', '1').lower() in VALID_BOOL_VALUES
 
-    source = request.args.get('source', 'fundsexplorer').lower()
-
-    print(f'---> [get_fii_data_by] Start params => Delete cache: {should_delete_cache}, Clear cache: {should_clear_cache}, Use cache: {should_use_cache}, Source: {source}, ticker: {ticker}')
+    source = request.args.get('source', 'all').lower()
 
     if should_delete_cache:
-        print(f'----> [get_fii_data_by] Deleting cache')
         delete_cache()
-        print(f'----> [get_fii_data_by] Cache deleted')
 
     if should_use_cache and not should_delete_cache:
-        print(f'----> [get_fii_data_by] Using cache')
         cached_data = read_cache(ticker, should_clear_cache)
 
-        print(f'----> [get_fii_data_by] Found on cache: {cached_data}')
         if cached_data:
             return jsonify({'data': cached_data, 'source': 'cache'}), 200
 
-    print(f'---> [get_fii_data_by] NOT using cache')
-
     data = request_fii_by(ticker, source)
 
-    print(f'---> [get_fii_data_by] Sucess getting data: {data}')
-
     if should_use_cache and not should_delete_cache and not should_clear_cache:
-        print(f'----> [get_fii_data_by] Saving cache')
         write_to_cache(ticker, data)
-        print(f'----> [get_fii_data_by] Cached saved')
 
     return jsonify({'data': data, 'source': 'fresh'}), 200
 
