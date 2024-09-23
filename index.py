a064b3b5dcd22e27981e3a4f2b8644d78dc7e853
SrDiegoH <diegohenrique4222@gmail.com>
2024-09-23 00:10:10 -0300

    Update index.py

--- index.py
+++ index.py
@@ -106,11 +106,13 @@ def get_data_from_fundsexplorer_by(ticker):
 
     response = request_get(f'https://www.fundsexplorer.com.br/funds/{ticker}', headers)
 
+    print(data_as_text)
+
     data_as_text = get_substring(response, 'var dataLayer_content =', 'dataLayer.push(')
 
     if data_as_text:
         return None
-    print(data_as_text)
+
     data_as_json = json.loads(data_as_text.rstrip(';'))['pagePostTerms']['meta']
 
     return convert_fundsexplorer_data(data_as_json)
