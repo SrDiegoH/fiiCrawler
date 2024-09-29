3531288cd4a811e47f1f70ee18c47985e0ac99f3
SrDiegoH <diegohenrique4222@gmail.com>
2024-09-29 13:54:29 -0300

    Update index.py

--- index.py
+++ index.py
@@ -276,4 +276,5 @@ def get_fii_data_by(ticker):
     return jsonify({'data': data, 'source': 'fresh'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    is_debug = os.getenv('IS_DEBUG', False)
+    app.run(debug=is_debug)
